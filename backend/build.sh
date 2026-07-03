e11fcf81d837e2291b63bef32b7259d354622160
ZiyodullaOtabaev <ziyodullaotabayev999@gmail.com>
2026-07-03 12:34:09 +0500

    Add superuser creation to build script

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,16 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+
+# Superuser yaratish (agar mavjud bo'lmasa)
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='Ziyodulla').exists():
+    u = User.objects.create_superuser(username='Ziyodulla', email='ziyodullamee@gmail.com', password='Ziyodulla2003!')
+    u.email_verified = True
+    u.save()
+    print('Superuser yaratildi')
+else:
+    print('Superuser allaqachon mavjud')
+"
